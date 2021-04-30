7545cfcf54883bb10f39839e153e1ea260a39b09
Juan L Gamella <juangamella@gmail.com>
2021-04-30 19:29:02 +0200

    Initial commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+Python template module
+======================
---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='python_template',
+    version='0.0.1',
+    author='Juan L Gamella',
+    author_email='juangamella@gmail.com',
+    packages=['template', 'template.test'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/template/',
+    license='LICENSE.txt',
+    description='Python template module',
+    long_description=open('README.txt').read(),
+    install_requires=[]
+)
